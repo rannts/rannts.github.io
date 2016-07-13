8e39389c58877b335ac8fb6388a116e0523c8034
9seconds <nineseconds@yandex.ru>
2016-07-13 19:47:10 +0300

    Correct problems with plugins

--- rannts/packages/yandexmaps/lektor_yandexmaps.py
+++ rannts/packages/yandexmaps/lektor_yandexmaps.py
@@ -3,24 +3,30 @@
 
 import urlparse
 
+import jinja2
+
 from lektor.pluginsystem import Plugin
 
 
 TEMPLATE = (
     '<script type="text/javascript" charset="utf-8"'
-    'async src="https://api-maps.yandex.ru/services/constructor/1.0/js/'
+    'async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?'
     'sid={constructor}&width=100%&height=500&lang=ru_RU&sourceType={source}'
     '&scroll=true"></script>'
 ).strip()
 
 
-def parse_yandexmaps(value):
+@jinja2.evalcontextfilter
+def parse_yandexmaps(ctx, value):
     url = urlparse.urlparse(value.url)
     query = urlparse.parse_qs(url.query)
     code = TEMPLATE.format(
         constructor=query["um"][-1].split(":")[-1],
         source=query["source"][-1]
     )
+    if ctx.autoescape:
+        code = jinja2.Markup(code)
+    print code
 
     return code
 
