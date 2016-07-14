5bd23ed5ae3b65f120e68528ea3c4f4717f78daf
9seconds <nineseconds@yandex.ru>
2016-07-14 23:19:05 +0300

    Add html minification to the gulpfile

--- rannts/gulpfile.js
+++ rannts/gulpfile.js
@@ -6,6 +6,7 @@ var DESTINATION = "assets/static",
     autoprefixer = require("autoprefixer"),
     postcss = require("gulp-postcss"),
     cssnano = require("cssnano"),
+    htmlmin = require("gulp-html-minifier"),
     optipng = require("gulp-optipng");
 
 
@@ -51,3 +52,26 @@ gulp.task("bundle_images", function() {
         .pipe(optipng(options))
         .pipe(gulp.dest(DESTINATION + "/images"));
 })
+
+
+gulp.task("bundle_html", function() {
+  gulp.src('./qqq/**/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      collapseInlineTagWhitespace: true,
+      decodeEntities: true,
+      removeAttributeQuotes: true,
+      removeComments: true,
+      removeEmptyAttributes: true,
+      removeEmptyElements: false, // required for burger menu
+      removeOptionalTags: true,
+      removeRedundantAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      sortAttributes: true,
+      sortClassName: true,
+      useShortDoctype: true
+    }))
+    .pipe(gulp.dest('./qqq'))
+});
