499633ee515b7998a2efffa9c82686d66b6caa00
9seconds <nineseconds@yandex.ru>
2016-08-18 18:47:40 +0300

    Add opengraph

--- packages/micawber/lektor_micawber.py
+++ packages/micawber/lektor_micawber.py
@@ -1,11 +1,31 @@
 # -*- coding: utf-8 -*-
 
 
+import posixpath
+import urlparse
+
 import jinja2
 import micawber
 import micawber.contrib.mcflask
 
 from lektor.pluginsystem import Plugin
+
+
+@jinja2.evalcontextfilter
+def youtube_code(ctx, value):
+    parsed = urlparse.urlsplit(value)
+    if "youtube.com" in parsed.netloc:
+        query = urlparse.parse_qs(parsed.query)
+        vcode = query["v"][-1]
+        value = "{scheme}://{netloc}".format(
+            scheme=parsed.scheme,
+            netloc=posixpath.join(parsed.netloc, "v", vcode)
+        )
+
+    if ctx.autoescape:
+        value = jinja2.Markup(value)
+
+    return value
 
 
 class MicawberPlugin(Plugin):
@@ -15,3 +35,4 @@ class MicawberPlugin(Plugin):
     def on_setup_env(self, **extra):
         providers = micawber.bootstrap_basic()
         micawber.contrib.mcflask.add_oembed_filters(self.env, providers)
+        self.env.jinja_env.filters["youtube_code"] = youtube_code
