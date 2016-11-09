46d20e210735ad3041153a3c34f731710e108306
9seconds <nineseconds@yandex.ru>
2016-11-09 08:47:04 +0300

    Add base of the plugin

---
+++ packages/calendar/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='lektor-calendar',
+    version='0.1',
+    author=u'Sergey Arkhipov',
+    author_email='nineseconds@yandex.ru',
+    license='MIT',
+    py_modules=['lektor_calendar'],
+    entry_points={
+        'lektor.plugins': [
+            'calendar = lektor_calendar:CalendarPlugin',
+        ]
+    }
+)
